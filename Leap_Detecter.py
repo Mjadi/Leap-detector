d4602d5365cd6e9832be210ccf9c895775d91f42
Mjadi <85009835+Mjadi@users.noreply.github.com>
2021-07-14 11:38:31 +0530

    Add files via upload

---
+++ Leap_Detecter.py
@@ -0,0 +1,33 @@
+
+# class Year:
+
+#     @staticmethod
+
+#     def leap_Det(y):
+#         y=int(y)
+#         if y%4==0:
+#             print('Leap year detected!')
+
+#         else:
+#             print('Nah its just 365 days')
+
+# Walo=Year()
+# Walo.leap_Det(2021)
+
+# We can do it without static method
+
+# class Year:
+
+#     def leap_Det(self, y):
+#         self.y=int(y)
+#         if self.y%4==0:
+#             print('Leap year detected!')
+
+#         else:
+#             print('Nope!')
+
+# Eag=Year()
+# Eag.leap_Det(2020)
+
+
+
